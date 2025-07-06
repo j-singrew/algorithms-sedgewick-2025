c45ec9b3f12601915f164ccee69f2184d8118e77
topshager <rem28777@gmail.com>
2025-07-06 22:26:08 +0200

    Update Dijkstra’s_Two-Stack_Algorithm.py

--- Dijkstra’s_Two-Stack_Algorithm.py
+++ Dijkstra’s_Two-Stack_Algorithm.py
@@ -1,29 +1,39 @@
-graph = {
-   "A": {"B": 3, "C": 3},
-   "B": {"A": 3, "D": 3.5, "E": 2.8},
-   "C": {"A": 3, "E": 2.8, "F": 3.5},
-   "D": {"B": 3.5, "E": 3.1, "G": 10},
-   "E": {"B": 2.8, "C": 2.8, "D": 3.1, "G": 7},
-   "F": {"G": 2.5, "C": 3.5},
-   "G": {"F": 2.5, "E": 7, "D": 10},
-}
+from collections import deque
+sum = "( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )"
 
-class Gragh:
-    def __init__(self,graph:dict = {}):
-        self.graph = graph
 
-    
-    def add_edge(self,node1,node2,weight):
-        if node1 not in self.graph:
-            self.graph[node1] = {}
-        self.graph[node1][node2] = weight
-    
-    def shortes_distances(self,source:str):
-        distances = {node:float("inf") for node in self.graph}
-        distances[source] = 0
-        for node in graph.key():
-            
+
+
+
+def Two_Stack_Alg(expr):
+    operand = deque()
+    number = deque()
+    tokens = sum.split()
+    for token in tokens:
+        if token == '(':
+            continue
+        if token in ['+','*','-','/']:
+            operand.append(token)
+        elif token == ')':
+            op = operand.pop()
+            val1 = number.pop()
+            val2 =number.pop()
+            if op == '+':
+                 number.append(val1+val2)
+            if op == '*':
+                number.append(val1*val2)
+            if op == '-':
+                number.append(val1-val2)
+            if op == '/':
+                number.append(val1/val2)
+        else:
+            number.append(float(token))
+    return number.pop()
+
+
+
+
+
         
-G = Gragh(graph=graph)
 
-G.graph
+
