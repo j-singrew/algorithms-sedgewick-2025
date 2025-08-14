dc4d65ee57f5451c47a0e9344bae67fce613bc50
topshager <rem28777@gmail.com>
2025-08-15 00:20:10 +0200

    Update weighted_quickunion.py

--- weighted_quickunion.py
+++ weighted_quickunion.py
@@ -17,3 +17,36 @@ Implement the weighted Quick-Find algorithm in code.
 
 Explain/observe the performance improvement.
 """
+class QuickUnionUF:
+
+    def __init__(self,n):
+        self.id = list(range(n)) #creates 10 number of id's
+        self.count = n #number of components
+
+    
+    def root(self,i):
+        while i != self.id[i]: #while i does not equal its idex position value
+            i = self.id[i] #set i to the value of  index
+        return i 
+    
+    def connected(self,p,q):
+        return self.root(p) == self.root(q) #this will retur true  or false 
+    
+    def union(self,p,q):
+        i = self.root(p)
+        j = self.root(q)
+        if i == j:
+            return #return true 
+        self.id[i] = j
+        self.count -=1
+
+    def __str__(self):
+        return f"Parent links: {self.id}, Components: {self.count}"
+    
+    
+uf = QuickUnionUF(10)
+uf.union(3, 4)
+uf.union(4, 9)
+print(uf.connected(3, 9))  # True
+print(uf)
+
