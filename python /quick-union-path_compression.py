b3640740f0e3aaacd9b1e17529e6b423d78b4fa3
topshager <rem28777@gmail.com>
2025-08-16 22:20:58 +0200

    Update quick-union-path_compression.py

--- python /quick-union-path_compression.py
+++ python /quick-union-path_compression.py
@@ -1,27 +1,31 @@
-#the one with the tree 
-"""
-Weighted quick-union with path compression. Modify weighted quick-union
-(Algorithm 1.5) to implement path compression, as described in Exercise 1.5.12.
-Give a sequence of input pairs that causes this method to produce a tree of height 4.
-Note : The amortized cost per operation for this algorithm is known to be bounded by a
-function known as the inverse Ackermann function and is less than 5 for any conceivable
-practical value of N.
-"""
-def __init__(self,n):
-    self.parent = list(range(n))
-    self.size = 1
+class weighted_quick_find:
+    def __init__(self,n):
+        self.parent = list(range(n))
+        self.size = [1]*n
 
+    def find(self,i):
 
-def root(self,p):
-    while p != self.parent[p]:
-        p = self.parent[p]
-    return p 
+        if i == self.parent[i]:
+            return i
+        
+        self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+    
+    def union(self,p,q):
 
+        rootQ = self.find(q)
+        rootP = self.find(p)
 
+        if rootQ == rootP:
+            return 
+        
+        #combining root's 
+        if self.size[rootP] < self.size[rootQ]:
+               
+               self.parent[rootP] = rootQ
+               self.size[rootQ] += self.size[rootP]
             
+        else:
 
-
-
-
-
-
+            self.parent[rootQ] = rootP
+            self.size[rootP] += self.size[rootQ]
